f2554fb72ee670c94e236e153e99120f98c747c7
“fiwallsld” <thanhpromu@gmail.com>
2024-10-15 00:08:33 +0700

    First commit

--- clien/src/components/hook/useFormatDate.js
+++
@@ -1,9 +0,0 @@
-function formatDate(date) {
-  const subDate = date.split("T")[0].split("-");
-
-  const day = +subDate[2] + 1;
-  const res = `${day < 10 ? "0" : ""}${day}/${subDate[1]}/${subDate[0]}`;
-  return res;
-}
-
-export default formatDate;
